15a29dc595e8e5c10a504a2a2427a6d7163a2a4d
atif shaikh <shaikhatif456a@gmail.com>
2023-05-17 11:15:11 +0530

    some changes are done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import './Components/Header'
 import Header from './Components/Header';
+import Home from './Components/Home';
 
 function App() {
   return (
     <div className="App">
       hello  guys
     <Header/>
+    <Home/>>
      
     </div>
   );
