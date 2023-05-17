aac9cbf7d31b99598f4e3e625df395ebdb2394c0
atif shaikh <shaikhatif456a@gmail.com>
2023-05-17 11:53:32 +0530

    increment and decrement button added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React , {useState} from "react";
+
 
 const Home = () => {
-    let inputValue;
+    const [inputValue, setInputValue]= useState(0);
+
+const increment=()=>{
+setInputValue(inputValue + 1);
+};
+const decrement=()=>{
+    setInputValue( inputValue - 1);
+}
+
+    const btnStyle={
+        padding :"20px",
+        border: "none",
+        backgroundColor:"#f1f1f1",
+        margin:"10px"
+        
+    }
+
 
   return (
     <div>
       <input
         style={{ padding: 20, border: "1px solid rgba(0,0,0,0.4)" }}
-        type="text"
+        type="number"
         placeholder="enter something"
-        onChange={(e)=>{
-          inputValue =e.target.value;
-          console.log(inputValue);
-        }}
+        value={inputValue}
+        readOnly
+
+      
+        
       />
+      <button style={btnStyle} onClick={increment}>+</button>
+      <button style={btnStyle} onClick={decrement}>-</button>
     </div>
   );
 };
